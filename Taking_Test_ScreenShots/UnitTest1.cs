fcc56eb1ceae67359459546dac05f64ecab0b4f0
Andersonpaul <andersonudokoro@yahoo.com>
2015-02-16 15:59:46 +0000

    Capturing Websites of Screenshots

---
+++ Taking_Test_ScreenShots/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing.Imaging;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Taking_Test_ScreenShots
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+        [TestInitialize]
+        public void setup()
+        {
+            driver=new FirefoxDriver();
+        }
+        
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver.Navigate().GoToUrl("http://google.com/");
+            
+            Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
+            ss.SaveAsFile("scrnshot", ImageFormat.Jpeg);
+        }
+    }
+}
