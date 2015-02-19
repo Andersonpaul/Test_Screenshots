9cc82f742bec906039e0d23314c02772330547b2
Andersonpaul <andersonudokoro@yahoo.com>
2015-02-19 08:48:01 +0000

    Perfoming Explicit Waits via 'WebDriverWait' Class

--- Taking_Test_ScreenShots/UnitTest1.cs
+++ Taking_Test_ScreenShots/UnitTest1.cs
@@ -2,6 +2,7 @@
 using System.Drawing.Imaging;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Taking_Test_ScreenShots
@@ -19,10 +20,15 @@ namespace Taking_Test_ScreenShots
         [TestMethod]
         public void TestMethod1()
         {
-            driver.Navigate().GoToUrl("http://google.com/");
-            
+            driver.Url = "http://google.com/";
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromMinutes(4));
+            var image = wait.Until(d =>
+                {
+                    var img = driver.FindElement(By.Id("hplogo"));
+                    return img;
+                });
             Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
-            ss.SaveAsFile("scrnshot", ImageFormat.Jpeg);
+            ss.SaveAsFile("C://users/chidie/desktop/scrnshot1", ImageFormat.Jpeg);
         }
     }
 }
